9787b74675d82d1e372062b03f67851aa16e1c04
Carlo Andoni <caranzargar98@gmail.com>
2022-01-15 00:22:29 -0500

    Navbar peqrincipal

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface MenuItem{
+  ruta: string;
+  nombre: string;
+}
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    `
+    li:hover{
+      color: white;
+      border-color: #8D98BB;
+      font-size: 1.1rem;
+      background-color: #636df8;
+      transition: 400ms;
+      cursor: pointer
+    }
+    `
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
+  menuItems: MenuItem[] = 
+  [
+    {
+      ruta: '/mapas/fullscreen',
+      nombre: 'FullScreen'
+    },
+    {
+      ruta: '/mapas/zoom-range',
+      nombre: 'Zoom Range'
+    },
+    {
+      ruta: '/mapas/marcadores',
+      nombre: 'Marcadores'
+    },
+    {
+      ruta: '/mapas/propiedades',
+      nombre: 'Propiedades'
+    }
+];
 
-  ngOnInit(): void {
-  }
 
 }
